0caa290f332ed25a97f8dc96ddc4e3123e67ac59
vitorsoligo <127339888+vitorsoligo@users.noreply.github.com>
2024-05-16 08:57:36 -0300

    Add files via upload

---
+++ front/sistema-login/login-frontend/src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../App.css'
+
+function Home() {
+    return(
+    <div>
+        <h1>Bem vindo a Home</h1>
+    </div>
+    )
+}
+
+export default Home;
